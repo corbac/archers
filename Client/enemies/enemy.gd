5483d5ca31152dea7ec109a94fe8ac6c752c59d1
yserir <yserir@gmail.com>
2020-05-11 15:49:49 +0200

    test

--- Client/enemies/enemy.gd
+++ Client/enemies/enemy.gd
@@ -1,10 +1,6 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var life : int = 3
 var damage = 2
 var armor = 1
@@ -16,7 +12,7 @@ var exp_given = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
